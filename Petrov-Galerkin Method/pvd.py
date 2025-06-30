70fea6e614ddb686c6eebddc1fc7094bbe3a5348
Felipe Santos Araujo <lipepiata@gmail.com>
2025-06-30 16:20:10 -0300

    30/06
    Add Petrov - Galerkin Least Squares methods

--- Petrov-Galerkin Method/pvd.py
+++ Petrov-Galerkin Method/pvd.py
@@ -2,11 +2,11 @@ import os
 from glob import glob
 
 # Caminho onde estão seus arquivos .vtu
-pasta = "D:/Documentos/Permanent-heat-conduction"
-arquivos = sorted(glob(os.path.join(pasta, "resultsQ100_t*.vtu")))
+pasta = "D:/Documentos/Petrov-Galerkin Method/Data"
+arquivos = sorted(glob(os.path.join(pasta, "results100_t=*.vtu")))
 
 # Nome do arquivo de saída .pvd
-saida_pvd = os.path.join(pasta, "results_adimensionalQ_100.pvd")
+saida_pvd = os.path.join(pasta, "results100.pvd")
 
 # Geração do arquivo PVD
 with open(saida_pvd, "w") as f:
@@ -15,7 +15,7 @@ with open(saida_pvd, "w") as f:
     for arq in arquivos:
         nome = os.path.basename(arq)
         # Extrai o valor do tempo do nome do arquivo, ex: arquivo_t0.01.vtu
-        tempo = nome.replace("resultsQ100_t", "").replace(".vtu", "")
+        tempo = nome.replace("results100_t=", "").replace(".vtu", "")
         f.write(f'    <DataSet timestep="{tempo}" group="" part="0" file="{nome}"/>\n')
     f.write('  </Collection>\n')
     f.write('</VTKFile>\n')
